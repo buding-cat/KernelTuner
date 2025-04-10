12a4af7c2d7ea4bb80cb953095d9015463f1c96d
buding-cat <aiccone00@gmail.com>
2025-04-10 10:44:30 +0800

    Update optimize_kernel.sh

--- optimize_kernel.sh
+++ optimize_kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/ > /etc/sysctl.conf
+curl -fsSL https://raw.githubusercontent.com/buding-cat/KernelTuner/refs/heads/main/sysctl.conf > /etc/sysctl.conf
 sysctl -p
